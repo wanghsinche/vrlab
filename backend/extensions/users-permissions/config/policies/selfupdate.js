cff633eefa8cac4216b5f4eba443c92513884848
author <author@fourf>
2021-09-21 01:39:17 +0800

    fix: update policy

--- backend/extensions/users-permissions/config/policies/selfupdate.js
+++ backend/extensions/users-permissions/config/policies/selfupdate.js
@@ -2,7 +2,7 @@
 
 const handleErrors = (ctx, err = undefined, type) => {
     throw strapi.errors[type](err);
-  };
+};
   
 
 module.exports = async (ctx, next) => {
@@ -14,6 +14,11 @@ module.exports = async (ctx, next) => {
         ctx.request.body, ctx.params, 
         ctx.state.user.role.name
     );
+    const allowField = ['email', 'password', 'realname','realid','class'];
+    const allowSet = new Set(allowField);
+    if (!Object.keys(ctx.request.body).every(el=>allowSet.has(el))){
+        return handleErrors(ctx, `only allow to modify ${allowField}, but got ${Object.keys(ctx.request.body)}`, 'forbidden');
+    }
 
     const id = ctx.params.id;
 
@@ -31,5 +36,5 @@ module.exports = async (ctx, next) => {
     //     return await next();
     // }
 
-    return handleErrors(ctx, undefined, 'forbidden');
+    return handleErrors(ctx, 'forbid to update others', 'forbidden');
 };
