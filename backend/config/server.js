aa58e286ff1b3fe43555cca277da4b8f94d11b1d
author <author@fourf>
2021-10-01 23:48:54 +0800

    fix: modify url

--- backend/config/server.js
+++ backend/config/server.js
@@ -1,7 +1,8 @@
+console.log('[strapi env]', process.env);
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
-  url: env.int('URL', 'https://vrlab.heroku.com'),
+  url: env('URL', ''),
   admin: {
     auth: {
       secret: env('ADMIN_JWT_SECRET', 'd06b51d98451d217c9a37cebe2edb309'),
