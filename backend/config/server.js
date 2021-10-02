da0634e2305df8e876cddc60740ffd67a58dc1da
author <author@fourf>
2021-10-02 13:23:38 +0800

    fix: always 1337

--- backend/config/server.js
+++ backend/config/server.js
@@ -1,6 +1,6 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
+  port: 1337, //env.int('PORT', 1337),
   url: env('URL', ''),
   admin: {
     auth: {
