a224f0efba102006be54e3f269884ae8d275e6c2
AAW_e63d <wang0xinzhe@gmail.com>
2022-05-10 13:29:33 +0000

    update build.sh.

--- build.sh
+++ build.sh
@@ -1,7 +1,9 @@
 #sudo docker build -t qingong:last .
 sudo docker run \
 --env PORT=80 \
---env URL=http://106.55.166.168/api \
+--env URL=http://0.0.0.0/api \
 -p 8080:80 \
 -v /home/ubuntu/data:/app/backend/data \
-wanghsinche/vrlab:73b2742a1fa702f2d3593327a08237b935757ad7 
+#qingong:last
+wanghsinche/vrlab:73b2742a1fa702f2d3593327a08237b935757ad7
+ 
