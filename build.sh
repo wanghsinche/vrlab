6fe00a424b9649c09dc8bd5a8c53fd652d253add
wanghsinche <wanghsinche@hotmail.com>
2022-05-19 12:11:59 +0000

    fix: fix the script

--- build.sh
+++ build.sh
@@ -2,6 +2,6 @@
 sudo docker run \
 --env PORT=80 \
 --env URL=http://106.55.166.168/api \
--p 80:80 \
+-p 8080:80 \
 -v /home/ubuntu/data:/app/backend/data \
-qingong:last 
+wanghsinche/vrlab:73b2742a1fa702f2d3593327a08237b935757ad7 
