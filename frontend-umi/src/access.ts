b0d942cf4bc305da972d27a3edb5a0ebabf9dcc9
author <author@fourf>
2022-06-08 00:57:05 +0800

    fix: access function

--- frontend-umi/src/access.ts
+++ frontend-umi/src/access.ts
@@ -1,7 +1,11 @@
 import { MeQuery } from '@/generated/graphql';
 export default function(initialState?: { me: MeQuery['me']}) {
+    console.log('initialize access');
     if (!initialState) {
-        return {};
+        return {
+          forStudent: true,
+          forTeacher: false
+        };
     }
 
     const { me } = initialState;
