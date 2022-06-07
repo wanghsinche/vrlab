aedbc32dd4c60ce088c96c60b4a06c8fccb61be4
author <author@fourf>
2022-06-08 01:23:41 +0800

    fix: fuck you umi update

--- frontend-umi/.umirc.ts
+++ frontend-umi/.umirc.ts
@@ -4,6 +4,9 @@ export default defineConfig({
   nodeModulesTransform: {
     type: 'none',
   },
+  access:{
+    strictMode: false
+  },
   routes: [
     { path: '/', component: '@/pages/index' },
     { path: '/sign', component: '@/pages/sign',
